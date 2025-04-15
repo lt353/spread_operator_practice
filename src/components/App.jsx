00ed713ee5c290a81097c725a379439be7ddc080
Lindsay Trenton <lt353@hawaii.edu>
2025-04-15 11:05:12 -1000

    Turned into a todo list where we store the list items in a array that displays as a bullet point after user enters in item and clicks Add. Uses concepts of useState, mapping, event handling, and spread operator.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    })
+    setInputText(""); // Clear the input field after adding the item
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>This is a simple template react app.</p>
-      <p>Feel free to modify it as you like.</p>
-      <p>Happy coding!</p>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input 
+          value={inputText}
+          onChange={handleChange}
+          type="text" 
+        />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => <li>{todoItem}</li>)}
+        </ul>
+      </div>
     </div>
   );
 }
@@ -14,3 +44,8 @@ function App() {
 export default App;
 
 
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
